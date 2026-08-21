b7ef136c9fb4c176165924af946ded4860c14e24
peterintech <ogbonnapete210@gmail.com>
2026-08-21 18:33:01 +0100

    feat: database setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/peterintech/psocial/internal/db"
 	"github.com/peterintech/psocial/internal/env"
 	"github.com/peterintech/psocial/internal/store"
 )
@@ -14,7 +15,21 @@ func main() {
 
 	cfg := config{
 		addr: fmt.Sprintf(":%s", env.GetEnv("PORT", "8080")),
+		db: dbConfig{
+			addr:         env.GetEnv("DB_ADDR", "postgres://postgres:postgres@localhost:5432/psocial?sslmode=disable"),
+			maxOpenConns: env.GetEnvAsInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetEnvAsInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetEnv("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+
+	if err != nil {
+		log.Panicf("Error connecting to the database: %v", err)
+	}
+
+	defer db.Close()
 
 	store := store.NewStorage(nil)
 
